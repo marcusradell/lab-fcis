c153846ecb4a729314b94284f8f105be541ae00b
Marcus Rådell <marcus@radell.net>
2024-10-18 09:41:07 +0200

    implement test for a tweet already on the timeline

--- src/index.test.ts
+++ src/index.test.ts
@@ -14,8 +14,8 @@ export function createTimeline(tweets: Tweet[]) {
     },
     add(tweetsToAdd: Tweet[]) {
       const existingIds = tweets.map(({ tid }) => tid);
-      const newTweets = tweetsToAdd.filter((tweetToAdd) =>
-        existingIds.includes(tweetToAdd.tid),
+      const newTweets = tweetsToAdd.filter(
+        (tweetToAdd) => !existingIds.includes(tweetToAdd.tid),
       );
       return createTimeline([...tweets, ...newTweets]);
     },
@@ -24,4 +24,33 @@ export function createTimeline(tweets: Tweet[]) {
 
 test("Empty Timeline", () => {
   deepEqual(createTimeline([]).getTweets(), []);
+});
+
+test("Timeline initialized with a single tweet", () => {
+  const tweet = { tid: "#tid", username: "#username", text: "#text" };
+
+  deepEqual(createTimeline([tweet]).getTweets(), [tweet]);
+});
+
+test("Tweet added to timeline", () => {
+  const tweet = {
+    tid: "abc",
+    username: "My",
+    text: "Look at my food!",
+  };
+  const result = createTimeline([]).add([tweet]).getTweets();
+
+  deepEqual(result, [tweet]);
+});
+
+test("Tweet added to a timeline that already has the tweet", () => {
+  const tweet = {
+    tid: "alongidwithnumbersandletters123456",
+    username: "twitterdiedwithelonmusk",
+    text: "ibustneedtopostregularlyforthealgo",
+  };
+
+  const result = createTimeline([tweet]).add([tweet]).getTweets();
+
+  deepEqual(result, [tweet]);
 });
