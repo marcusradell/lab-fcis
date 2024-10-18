535e41a9dcd076c3a2d22b350dace7c9d421b853
Marcus Rådell <marcus@radell.net>
2024-10-18 09:20:23 +0200

    implement timeline according to fcis video

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,24 @@
 import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
+type Tweet = {
+  tid: string;
+  username: string;
+  text: string;
+};
+
+export function createTimeline(tweets: Tweet[]) {
+  return {
+    add(tweetsToAdd: Tweet[]) {
+      const existingIds = tweets.map(({ tid }) => tid);
+      const newTweets = tweetsToAdd.filter((tweetToAdd) =>
+        existingIds.includes(tweetToAdd.tid),
+      );
+      return createTimeline([...tweets, ...newTweets]);
+    },
+  };
+}
+
 test("It works!", () => {
   deepEqual(1, 1);
 });
